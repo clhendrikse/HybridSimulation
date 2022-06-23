f491aa5e05c5de11189800ab367d12510f0cd370
Chloe Hendrikse <70405404+clhendrikse@users.noreply.github.com>
2022-06-23 12:08:03 -0500

    updated as of 6/23/2022

--- SimulationCode4and8deme.R
+++ SimulationCode4and8deme.R
@@ -2,6 +2,9 @@
 
 library(diveRsity)
 library(adegenet)
+library(pophelper)
+library(xlsx)
+library(sjmisc)
 
 #_____________________________4 Demes____________________________________________________________________________________
 setwd("/Users/CHendrikse/Documents/fsc26_win64/4Demes/")
@@ -13,10 +16,6 @@ arp2gen("4Demes_1_1.arp")
 #turning file into a genind object
 deme4ind1 <- read.genepop("4Demes_1_1.gen", ncode = 3)
 deme4ind1@tab
-
-#checking values
-nLoc(deme4ind1)
-# ncol(deme4ind1@tab)
 
 individuals4demes1 <- seppop(deme4ind1,res.type="genind")
 names(individuals4demes1)
@@ -35,8 +34,8 @@ hybridsplusparents4demes1@pop
 final_genind4_1 <- repool(species1_1, species2_1, hybridsplusparents4demes1, species3_1, species4_1)
 final_genind4_1@pop
 #making function to store to vector
-final_genind_df4_1 <- genind2df(final_genind)
-actual_values4_1 <- as.vector(final_genind_df$pop)
+final_genind_df4_1 <- genind2df(final_genind4_1)
+actual_values4_1 <- as.vector(final_genind_df4_1$pop)
 actual_values4_1
 
 #writing values into text file
@@ -47,6 +46,7 @@ genind2structure(final_genind4_1, file="parentandhybrid", pops=FALSE)
 
 #4demes n=2------------------------------------------------------------------------------------------------------------------
 
+setwd("/Users/CHendrikse/Documents/fsc26_win64/4Demes/")
 #converting .arp file to a .gen file
 arp2gen("4Demes_1_2.arp")
 
@@ -85,9 +85,8 @@ is.vector(actual_values4_2)
 
 genind2structure(final_genind4_2, file="parentandhybrid4_2", pops=FALSE)
 
-
-
 #____________________________________________8 Demes___________________________________________________________________
+#_____________________________8 Demes_____________________________________________________________________________________
 setwd("/Users/CHendrikse/Documents/fsc26_win64/8Demes/")
 #8Demes n=1------------------------------------------------------------------------------------------------------------
 
@@ -131,11 +130,8 @@ is.vector(actual_values8_1)
 
 #convert to structure file
 genind2structure(final_genind8_1, file="parentandhybrid8_1", pops=FALSE)
-
-
-
 #8Demes n=2------------------------------------------------------------------------------------------------------------
-
+setwd("/Users/CHendrikse/Documents/fsc26_win64/8Demes/")
 #converting .arp file to a .gen file
 arp2gen("8Demes_1_2.arp")
 
@@ -176,12 +172,10 @@ is.vector(actual_values8_2)
 
 #convert to structure file
 genind2structure(final_genind8_2, file="parentandhybrid8_2", pops=FALSE)
+#----
 
 
-
-
-
-#genind to structure file code (citation in my drive)------------------------------------
+#___________genind to structure file code (citation in my drive)__________________________________________________
 genind2structure <- function(obj, file="", pops=FALSE){
   if(!"genind" %in% class(obj)){
     warning("Function was designed for genind objects.")
@@ -224,4 +218,199 @@ genind2structure <- function(obj, file="", pops=FALSE){
   write.table(tab, file=file, sep="\t", quote=FALSE, row.names=FALSE)
 }
 
-read.csv2("4deme/parentandhybridset/Results/parentandhybridset_run_1_f", skip = 55)
+  
+demeQmat <- readQ("4deme/parentandhybridset/Results4deme1/results4deme1")  
+str(demeQmat)  
+
+
+#function that takes the Q values and creates a new column that labels STRUCTURE's decision changing the value in this function should allow the other functions to still run
+structuredecision <- function(obj){
+  
+  #putting individuals in groups depending on what STRUCTURE decides (base deme=4)
+      #pure
+      pure1 <- rownames(obj[which(obj$Cluster1 > 0.9),])  
+      pure2 <- rownames(obj[which(obj$Cluster2 > 0.9),])
+      pure3 <- rownames(obj[which(obj$Cluster3 > 0.9),])
+      pure4 <- rownames(obj[which(obj$Cluster4 > 0.9),])
+      #hybrids
+      hybrids1 <- rownames(obj[which(obj$Cluster1 > 0.4 & obj$Cluster1 < .6),])
+      hybrids2 <- rownames(obj[which(obj$Cluster2 > 0.4 & obj$Cluster2 < .6),])
+      hybrids3 <- rownames(obj[which(obj$Cluster3 > 0.4 & obj$Cluster3 < .6),])
+      hybrids4 <- rownames(obj[which(obj$Cluster4 > 0.4 & obj$Cluster4 < .6),])
+      hybridinds <- c(hybrids1, hybrids2, hybrids3, hybrids4)
+      if(ncol(obj) == 8){
+        hybridinds <- c(hybrids5, hybrids6, hybrids7, hybrids8)  
+      }
+      hybridinds <- unique(hybridinds)
+      guesses <- c(pure1,pure2,pure3,pure4,hybridinds)
+#for 8 demes
+      
+      #creating base value
+      pure5<- 0
+      pure6<- 0
+      pure7<- 0
+      pure8<- 0
+    if(ncol(obj) == 8){
+      pure5 <- rownames(obj[which(obj$Cluster5 > 0.9),])  
+      pure6 <- rownames(obj[which(obj$Cluster6 > 0.9),])
+      pure7 <- rownames(obj[which(obj$Cluster7 > 0.9),])
+      pure8 <- rownames(obj[which(obj$Cluster8 > 0.9),])
+      
+      hybrids5 <- rownames(obj[which(obj$Cluster5 > 0.4 & obj$Cluster5 < .6),])
+      hybrids6 <- rownames(obj[which(obj$Cluster6 > 0.4 & obj$Cluster6 < .6),])
+      hybrids7 <- rownames(obj[which(obj$Cluster7 > 0.4 & obj$Cluster7 < .6),])
+      hybrids8 <- rownames(obj[which(obj$Cluster8 > 0.4 & obj$Cluster8 < .6),])
+      
+      
+      hybridinds <- c(hybridinds, hybrids5, hybrids6, hybrids7, hybrids8)
+      guesses <- c(pure1,pure2,pure3,pure4,hybridinds, pure5, pure6, pure7, pure8)
+    }
+      v <- 1
+      
+      #creating initial decisions object
+      if(row.names(obj)[1] %in% pure1) {
+        decision <- "pop1"
+        v <- v+1
+      }
+      if(row.names(obj)[1] %in% pure2) {
+        decision <- "pop2"
+        v <- v+1
+      }
+      if(row.names(obj)[1] %in% pure3) {
+        decision <- "pop3"
+        v <- v+1
+      }
+      if(row.names(obj)[1] %in% pure4) {
+        decision <- "pop4"
+        v <- v+1
+      }
+      if(row.names(obj)[1] %in% pure5) {
+        decision <- "pop5"
+        v <- v+1
+      }
+      if(row.names(obj)[1] %in% pure6) {
+        decision <- "pop6"
+        v <- v+1
+      }
+      if(row.names(obj)[1] %in% pure7) {
+        decision <- "pop7"
+        v <- v+1
+      }
+      if(row.names(obj)[1] %in% pure8) {
+        decision <- "pop8"
+        v <- v+1
+      }
+      if(row.names(obj)[1] %in% hybridinds) {
+        decision <- "hybrid"
+        v <- v+1
+      }
+      if(row.names(obj)[1] %nin% guesses) {
+        decision <- "unknown"
+        v <- v+1
+      }
+      
+      
+      
+      while(v <= length(obj$Cluster4)){
+        if(row.names(obj)[v] %in% pure1) {
+          decision <- c(decision, "pop1")
+          
+        }
+        if(row.names(obj)[v] %in% pure2) {
+          decision <- c(decision,"pop2")
+         
+        }
+        if(row.names(obj)[v] %in% pure3) {
+          decision <- c(decision,"pop3")
+         
+        }
+        if(row.names(obj)[v] %in% pure4) {
+          decision <- c(decision,"pop4")
+         
+        }
+        if(row.names(obj)[v] %in% pure5) {
+          decision <- c(decision,"pop5")
+          
+        }
+        if(row.names(obj)[v] %in% pure6) {
+          decision <- c(decision,"pop6")
+          
+        }
+        if(row.names(obj)[v] %in% pure7) {
+          decision <- c(decision,"pop7")
+          
+        }
+        if(row.names(obj)[v] %in% pure8) {
+          decision <- c(decision,"pop8")
+          
+        }
+        if(row.names(obj)[v] %in% hybridinds) {
+          decision <- c(decision,"hybrid")
+          
+        }
+        if(row.names(obj)[v] %nin% guesses) {
+          decision <- c(decision, "unknown")
+        
+        }
+         v <- v+1
+      }
+        
+      length(decision)
+        
+        obj$Decision <- decision
+      
+      tablewithdecision <- obj
+      return(tablewithdecision)
+
+}
+tabledecisions <- structuredecision(demeQmat$results4deme1)
+tabledecisions
+
+#function adding all true values
+AddingTrueGroup <- function(tablewithdecisions, lablevector){
+ tablewithdecisions$ActualVals <- lablevector
+  return(tablewithdecisions)
+}
+tablewithboth <- AddingTrueGroup(tabledecisions,actual_values4_1)
+
+#comparing groups and giving either TRUE or FALSE values depending on if STRUCTURE was correct in the individuals placement
+CompareQ <- function(tabletocompare){
+     bookmark <- 1
+     #creating initial comparison object
+     if(tabletocompare$Decision[1] == tabletocompare$ActualVals[1]){
+       bookmark <- bookmark +1
+       comparison <- c(TRUE)
+     }else{
+       comparison <- c(FALSE)
+       bookmark <- bookmark + 1
+     }
+     
+      #looping the table to make each comparison value
+     while(bookmark <= length(tabletocompare$Cluster1)){
+           if(tabletocompare$Decision[1] == tabletocompare$ActualVals[1]){
+             bookmark <- bookmark +1
+             comparison <- c(comparison, TRUE)
+           }else{
+             comparison <- c(comparison, FALSE)
+             bookmark <- bookmark + 1
+           }
+     }
+  
+     tabletocompare$Comparison <- comparison
+     newtable <- tabletocompare
+     return(newtable)
+}
+completetable <- CompareQ(tablewithboth)
+
+#Function calculating proportion of successes_________________________________________________________
+ProportionOfSuccess <- function(finaltable){
+  
+  numsuccess <- sum(finaltable$Comparison)
+  percentsuccess <- numsuccess/length(finaltable$Comparison)
+  
+  return(percentsuccess)
+  
+}
+SuccessProp <- ProportionOfSuccess(completetable)
+SuccessProp
+#outputs proportion of successes
