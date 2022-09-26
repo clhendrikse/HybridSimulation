2791a6d29956da031bf4648cec185cc915814b67
clhendrikse <ch881518@ohio.edu>
2022-09-25 20:23:09 -0500

    adding 20nds files

---
+++ Scenarios/5sp_10ms_20ns/Run6/runSTRUCT.sh
@@ -0,0 +1,16 @@
+#!/bin/bash/
+
+# To get the start processing time, get the current system time, taken at the beginning of the run, and print it
+now=$(date)
+echo "Current date/time: $now"
+# End processing time can be taken from the output file timestamps (i.e. nohup.out)
+
+# STRUCTURE command
+# K = 8, five replicates
+for r in {1..5} ;
+do
+   /usr/local/bin/structure -i parentandhybrid6.str -m mainparams.txt -e extraparams.txt -K 5 -o parentandhybrid5Deme_$r > Outputs/parentandhybrid5Deme_$r &
+   sleep 3s
+done
+
+echo "All runs started!"
